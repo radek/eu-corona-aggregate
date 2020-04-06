c4b867588d0b622504c7b24bf2eb6ecf306b58e6
Radek Czarnecki <radek.czarneck@gmail.com>
2020-04-05 23:00:58 -0400

    Add offset

--- aggregate.js
+++ aggregate.js
@@ -4,14 +4,17 @@
  *
  * @param {Object} data - collection of pairs (label, values)
  * @param {Array} group - group name followed by list of member labels
+ * @param {Number} offset - number of (first) values removed
  * @returns {Object}
  */
 
-module.exports = (data, group=[]) => {
+module.exports = (data, group=[], offset=0) => {
   let agregate = []
   let len = Object.keys(data).length
 
   return Object.keys(data).reduce((acc, cur, idx) => {
+    data[cur].splice(0, offset)
+
     acc[cur] = data[cur]
 
     if (group.length && group.includes(cur)) {
